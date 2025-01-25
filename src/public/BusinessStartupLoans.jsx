c54fc0741073b62205d14b47f39ce168b23b0dc9
Azam <azam78454@gmail.com>
2025-01-26 03:55:15 +0500

    Fix

--- src/public/BusinessStartupLoans.jsx
+++ src/public/BusinessStartupLoans.jsx
@@ -6,7 +6,7 @@ function BusinessStartupLoans() {
     <section className="w-[90%] mx-auto py-14">
 
       <div className="p-5  border-2 border-gray-200 rounded-lg shadow-lg dark:border-gray-700">
-        <h1 className="text-3xl mb-5 font-semibold">Wedding Loans</h1>
+        <h1 className="text-3xl mb-5 font-semibold">Business Startup Loans</h1>
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
           <Card link="/form/buyStall" head="Buy Stall" />
           <Card link="/form/advanceRentForShop" head="Advance Rent for Shop" />
